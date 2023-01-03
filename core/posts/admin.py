b939fa8419c51fdbd4f1d7fc2fec2a9de1f38e63
Zainab <118971015+ZZainabshaabneh@users.noreply.github.com>
2023-01-04 00:22:19 +0200

    Delete core directory

--- core/posts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Likes,Posts,User,Comments,Address
-admin.site.register(Posts)
-admin.site.register(Likes)
-admin.site.register(Comments)
-admin.site.register(Address)
-admin.site.register(User)
-
--- core/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from core.posts.views import PostRetrieveUpdateDestroyAPIView,PostListCreateAPIView
-
-urlpatterns=[
-    path(r'posts/<int:post_id>/',PostRetrieveUpdateDestroyAPIView.as_view()),
-    path(r'posts/', PostListCreateAPIView.as_view()),
-]
-
-
